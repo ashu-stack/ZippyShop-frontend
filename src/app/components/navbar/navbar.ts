308a1433ca9f51f8217ca5e6c205c2edc7a32adf
Ashutosh Shukla <ashutoshhh37@gmail.com>
2026-06-03 08:42:42 +0530

    View cart button added to the cart

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Products } from '../../../product';
+import { ProductService } from '../../services/product-service';
+import { Input } from '@angular/core';
+import { CartService } from '../../services/cart-service';
+
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-navbar',
   standalone:true,
   imports: [RouterModule],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css',
+  styleUrls: ['./navbar.css']
 })
-export class Navbar {}
+export class Navbar {
+
+  product !: Observable<Products>;
+
+  service = inject(ProductService)
+  cartService = inject(CartService)
+
+  @Input()
+  name !: string;
+
+  getProduct(){
+    
+    this.product = this.service.getProductByName(this.name)
+  }
+
+  setName(name: string){
+    this.name = name;
+  }
+
+   viewCart(){
+    this.cartService.getCart();
+  }
+
+
+}
