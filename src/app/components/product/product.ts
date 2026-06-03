dc584fdd9e941379cfd5552a10b8a8deff278795
Ashutosh Shukla <ashutoshhh37@gmail.com>
2026-06-03 16:06:04 +0530

    remove item functionality added

--- src/app/components/product/product.ts
+++ src/app/components/product/product.ts
@@ -1,19 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit,inject } from '@angular/core';
 import { Products } from '../../../product';
 import { Input } from '@angular/core';
 import { Counter } from '../counter/counter';
+import { CommonModule } from '@angular/common';
+import {AsyncPipe} from '@angular/common'
+import { CartService } from '../../services/cart-service';
 
 @Component({
   selector: 'app-product',
-  imports: [Counter],
+  imports: [Counter,CommonModule],
   templateUrl: './product.html',
   styleUrl: './product.css',
 })
-export class Product {
+export class Product  {
 
   @Input()
   product !: Products
 
+  service = inject(CartService)
+
+  addToCart(){
+    this.service.addToCart(this.product.name, 1)
+
+  }
+
+  removeItem(){
+    this.service.removeItem(this.product.name)
+  }
   
 
 }
